f9fc46bd8044ac95cbc500661a5b79b3ada7d189
isis1225 <60119155+isis1225@users.noreply.github.com>
2020-01-21 20:24:31 -0500

    Update app.py

--- App/app.py
+++ App/app.py
@@ -4,10 +4,14 @@ import csv
 from time import process_time 
 
 def loadCSVFile (file, lst, sep=";"):
+    print("Cargando archivo ....")
+    t1_start = process_time() #tiempo inicial
     with open(file) as csvfile:
         spamreader = csv.reader(csvfile, delimiter=sep)
-        for row in spamreader:
+        for row in spamreader: 
             lst.append(row)
+    t1_stop = process_time() #tiempo final
+    print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
 
 def printMenu():
     print("\nBienvenido")
@@ -19,10 +23,17 @@ def printMenu():
 
 def countMoviesByName(movieMame, lst):
     #Contar la cantidad de peliculas con titulo que inclue movieName
-    moviesCount=0
-    for mov in lst:
-        if movieMame.lower() in mov[5].lower(): #comparar con el titulo (columna 5)
-            moviesCount+=1
+    if len(lista)==0:
+        print("La lista esta vacía")  
+        return 0
+    else:
+        t1_start = process_time() #tiempo inicial
+        moviesCount=0
+        for mov in lst:
+            if movieMame.lower() in mov[5].lower(): #comparar con el titulo (columna 5)
+                moviesCount+=1
+        t1_stop = process_time() #tiempo final
+        print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
     return moviesCount
 
 def countGoodMoviesByDirector(lastName, lst):
@@ -31,33 +42,22 @@ def countGoodMoviesByDirector(lastName, lst):
     return 0
 
 
-
 lista = [] #instanciar una lista vacia
 while True:
     printMenu() #imprimir el menu de opciones en consola
     inputs =input('Seleccione una opción para continuar\n') #leer opción ingresada
     if len(inputs)>0:
         if int(inputs[0])==1: #opcion 1
-            t1_start = process_time() #tiempo inicial
-            print("Cargando archivo ....")
             loadCSVFile("Data/themoviesdb/SmallMoviesDetailsCleaned.csv", lista) #llamar funcion cargar datos
-            t1_stop = process_time() #tiempo final
             print("Datos cargados, "+str(len(lista))+" elementos cargados")
-            print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
         elif int(inputs[0])==2: #opcion 2
             if len(lista)==0:
                 print("La lista esta vacía")    
             else: print("La lista tiene "+str(len(lista))+" elementos")
         elif int(inputs[0])==3: #opcion 3
-            if len(lista)==0:
-                print("La lista esta vacía")  
-            else:
-                movieName =input('Ingrese parte del titulo de la pelicula\n')
-                t1_start = process_time() #tiempo inicial
-                moviesCount=countMoviesByName(movieName,lista)
-                t1_stop = process_time() #tiempo final
-                print("Peliculas que contienen '", movieName ,"' en el titulo: ",moviesCount)
-                print("Tiempo de ejecución ",t1_stop-t1_start," segundos")
+            movieName =input('Ingrese parte del titulo de la pelicula\n')
+            moviesCount=countMoviesByName(movieName,lista)
+            print("Peliculas que contienen '", movieName ,"' en el titulo: ",moviesCount)
         elif int(inputs[0])==4: #opcion 4
             lastName =input('Ingrese el apellido del director\n')
             moviesCount=countGoodMoviesByDirector(lastName,lista)
